2fb74f1bfa13908a8deda34b86fc60c71f877eb1
Laura <laura.heddleston@gmail.com>
2020-03-05 10:22:41 -0800

    Express server setup + dev dependencies setup

---
+++ challenge_3/README.md
@@ -0,0 +1,3 @@
+# CHECKOUT
+
+Goal: Build a single-page app that takes a user through a series of forms that simulate a shopping cart checkout experience.
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
 
+// CONTROLLERS
+const fetchData = (request, response) => {
+  console.log('Received a GET request');
+  response.status(200);
+  response.json();
+}
+const postData = (request, response) => {
+  console.log('Receieved a POST request');
+  response.status(200);
+  response.json();
+}
 
+// ROUTES
+router.get('/', fetchData);
+router.post('/', postData);
+
+// START
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
