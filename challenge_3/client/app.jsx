1cc76a4479a05a0720520367323b19898e3613ce
Laura <laura.heddleston@gmail.com>
2020-03-06 09:09:47 -0800

    Start first page setup

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,17 +1,35 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = "";
+    this.state = {
+      currentPage = 0;
+    };
+  }
+  handleClick() {
+    console.log('Clicked');
   }
   render() {
     return (
       <div>
         <h1>HEY {this.state = "Laura"}!</h1>
         <h2> What type of manimal are you? </h2>
+        <button className="button" type="button" onClick={handleClick}>Find Out Here</button>
       </div>
     )
   }
 };
+
+class FirstPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = "";
+  }
+  render() {
+    return (
+      <h1> Page One!</h1>
+    )
+  }
+}
 
 
 
